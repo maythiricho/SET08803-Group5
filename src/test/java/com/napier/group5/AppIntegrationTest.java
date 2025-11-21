62e6171bf09e4ad2013d5260c61e46115bb2fcce
KyawZayarMin1234 <kyawkzayae@gmail.com>
2025-11-21 22:04:34 +0630

    language_reports

--- src/test/java/com/napier/group5/AppIntegrationTest.java
+++ src/test/java/com/napier/group5/AppIntegrationTest.java
@@ -280,25 +280,6 @@ public class AppIntegrationTest {
         logger.removeHandler(handler);
     }
 
-    @Test
-    void runAllReportsProducesOutput() throws Exception {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Handler handler = new StreamHandler(baos, new SimpleFormatter());
-        logger.addHandler(handler);
-
-        App.main(new String[]{"localhost:3307", "60000"});
-
-        handler.flush();
-        String output = baos.toString();
-
-        assertTrue(output.contains("Country Reports"));
-        assertTrue(output.contains("City Reports"));
-        assertTrue(output.contains("Capital City Reports"));
-        assertTrue(output.contains("Language Reports"));
-        assertTrue(output.contains("32. Population by language"));
-
-        logger.removeHandler(handler);
-    }
 
     @Test
     void emptyResultStillPrintsHeader() throws Exception {
