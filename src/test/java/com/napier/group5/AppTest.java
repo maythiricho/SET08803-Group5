0136d61603e4f9281f8bdfee8e8c5c8b60bb0ed3
KyawZayarMin1234 <kyawkzayae@gmail.com>
2025-11-23 22:36:58 +0630

    capital_reports

--- src/test/java/com/napier/group5/AppTest.java
+++ src/test/java/com/napier/group5/AppTest.java
@@ -173,7 +173,7 @@ public class AppTest {
         });
     }
 
-    // ---------- demo output test (prints sample table) ----------.1.
+    // ---------- demo output test (prints sample table) ----------..1.
 
     @Test
     @DisplayName("Demo: prints sample world report table")
